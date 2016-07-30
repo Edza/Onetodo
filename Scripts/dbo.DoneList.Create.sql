86e43693ec2f0c993fdfc69ff5bfee0d9c112e17
Edza <edexdev@gmail.com>
2016-07-30 16:16:15 +0300

    Inital commit

---
+++ Scripts/dbo.DoneList.Create.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DoneList]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Date] Date NOT NULL,
+	[Dosage] DECIMAL(6,2) NOT NULL,
+	[Calories] INT NOT NULL,
+	[Vitamins] BIT NOT NULL,
+	[MoneySpent] INT NOT NULL
+)
